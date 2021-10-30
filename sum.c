054463a06020defda4f8009ffb1f52a8a81b6410
20P35A0115 <93194817+20P35A0115@users.noreply.github.com>
2021-10-30 12:06:23 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	 //variables
+	 int num1, num2, sum;
+	 
+	 //initialization
+	 num1 = 1232456;
+	 num2 = 7895665;
+	 
+	 //calculation
+	 sum = num1 + num2;
+	 
+	 //output
+	 printf("sum of %d and %d is %d",num1,num2,sum);
+}
